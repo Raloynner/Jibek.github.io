1b33afc95a31769f4bfa2c81306482bf30cb6f55
Raloynner <freeman190793@gmail.com>
2024-07-11 20:26:36 +0600

    add

--- js/global.js
+++ js/global.js
@@ -1,12 +1,27 @@
 $( function() {
     // $( "#tabs" ).tabs();
-    // $( "#tabs-production" ).tabs();
+    $( "#tabs-client, #tabs-services" ).tabs();
     // $( "#tabs-advantages" ).tabs();
     // $( ".product-block-content-left" ).tabs();
     // $( "#tab-history" ).tabs();
-    $( "#faq" ).accordion();
+    $( "#faq, #faq-2, .accordion" ).accordion({
+        heightStyle: "content",
+        collapsible: true,
+        active: false
+    });
     $('.counter').counterUp({
         delay: 20,
         time: 1500
+    });
+    $(".burger").click(function(){
+        $(".mobileMenu").toggleClass('active');
+    });
+    $(window).resize(function() {
+        //do something
+
+        var width = $(document).width();
+        if (width > 1200) {
+            $(".mobileMenu").removeClass('active');
+        }
     });
 } );
